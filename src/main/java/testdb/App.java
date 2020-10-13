eb5c7b6cb9758cb25a0271f71a026c4b82d74cfc
Elmedin <eldzafic@tsn.at>
2020-10-13 13:55:07 +0200

    Fehler behoben, verbindung aufbauen, schließen und ausgabe tabelle geht

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -1,11 +1,16 @@
 package testdb;
 
 import java.sql.*;
+import java.util.Calendar;
 
 /**
  * Hello world!
  */
 public class App {
+
+
+    private static Connection con;
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 /*        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -20,36 +25,49 @@ public class App {
         }
 
  */
-        Date date = new Date();
-        insertInvoice(verbindungAufbauen(), date, "beschreibung!", 12345.1, true);
+        //java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        //insertInvoice(verbindungAufbauen(), date, "beschreibung!", 12345.1, true);
 
-        verbindungClose(verbindungAufbauen());
+        verbindungAufbauen();
+        showInvoices();
 
     }
 
-    public static Connection verbindungAufbauen() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
-        return con;
+    public static void verbindungAufbauen() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+        } catch (Exception e) {
+            System.out.println("Verbindung konnte nicht hergestellt werden!");
+        }
     }
 
-    public static void verbindungClose(Connection con) throws SQLException {
-        con.close();
+    public static void verbindungClose(){
+        if(con != null) {
+            try {
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        else{
+            System.out.println("Keine Verbindung!");
+        }
     }
 
-    public static void showInvoices(Connection con)
+    public static void showInvoices()
     {
         try{
         Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select id, name, nachname from benutzer");
+            ResultSet rs = stmt.executeQuery("select id, idate, idescription, ivalue, ipaid from invoice");
             while (rs.next())
-                System.out.println(rs.getInt("id") + "  " + rs.getString("name") + " " + rs.getString("nachname"));
-            con.close();
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + " " + rs.getString(3) + " " +rs.getInt(4) + " " +rs.getString(5));
+            verbindungClose();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-
+/*
     public static void insertInvoice(Connection con, Date date, String description, double value, Boolean paid)
     {
         try {
@@ -65,7 +83,7 @@ public class App {
             System.out.println(e);
         }
     }
-
+*/
     public static void updateInvoice(int id, Date date, String description, double value, Boolean paid)
     {
 
@@ -74,6 +92,14 @@ public class App {
     public static void deleteInvoice(int id)
     {
 
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
     }
 
 }
